2f732a3f93f5f6733337653972eefe6c2f060326
philipdaquin <philipdaquin20@gmail.com>
2023-09-24 18:36:41 +0800

    improved argocd terraform files

--- argocd/1-helm.tf
+++ argocd/1-helm.tf
@@ -15,18 +15,6 @@ data "aws_eks_cluster" "cluster" {
 #   name = data.terraform_remote_state.eks.outputs.cluster_name
 # }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.eks.cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
-    client_key = aws_eks_cluster.cluster.client_key
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
-      command     = "aws"
-    }
-  }
-}
 
 resource "helm_release" "argocd" {
     repository          = "https://argoproj.github.io/argo-helm"
