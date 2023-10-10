05c4a21ef9c242d2564ba28f5cc1594fcc1ddf41
philipdaquin <philipdaquin20@gmail.com>
2023-10-10 16:27:56 +0800

    updated manifest

--- monitoring/deployment/build.sh
+++ monitoring/deployment/build.sh
@@ -6,10 +6,10 @@ cd user_deployment  && kubectl apply -f . && cd -
 cd api_gateway      && kubectl apply -f . && cd -
 cd discovery_service && kubectl apply -f . && cd -
 cd friend_deployment && kubectl apply -f . && cd -
-cd recommendation_deployment && kubectl apply -f . && cd -
+# cd recommendation_deployment && kubectl apply -f . && cd -
 
 cd redis            && bash build.sh && cd -
 cd postgres         && kubectl apply -f . && cd -
 cd mongodb_deployment && kubectl apply -f . && cd -
-cd neo4j && bash build-neo4j.sh && cd -
-cd kafka && kubectl apply -f . && cd - 
+# cd neo4j && bash build-neo4j.sh && cd -
+# cd kafka && kubectl apply -f . && cd - 
